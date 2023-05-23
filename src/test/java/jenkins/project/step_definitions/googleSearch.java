5e75bfba8b8a0b9925c2002ec199cd56fa33ac46
erdogank <112961176+erdogank@users.noreply.github.com>
2023-05-24 01:08:56 +0300

    first

--- src/test/java/jenkins/project/step_definitions/googleSearch.java
+++ src/test/java/jenkins/project/step_definitions/googleSearch.java
@@ -4,13 +4,25 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class googleSearch {
 
     @Given("user is on Google search page")
-    public void user_is_on_google_search_page() {
+    public void user_is_on_google_search_page() throws MalformedURLException {
+        RemoteWebDriver driver = new ChromeDriver();
 
-        ChromeDriver driver = new ChromeDriver();
+        String gridAddress = "52.55.145.210";
+        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setBrowserName("chrome");
+        driver = new RemoteWebDriver(url, desiredCapabilities);
+
+
 
         driver.get("https://www.google.com");
 
